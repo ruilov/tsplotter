486bad49ed50e6ad0f0351bd4dabacb319111b3f
Rui Viana <ruilov@gmail.com>
2016-07-08 11:30:50 -0400

    test

--- python_scripts/start_here.py
+++ python_scripts/start_here.py
@@ -6,19 +6,19 @@ from pricer.option import Option
 pricer.cme.init()
 
 for dt,val in RB.futures().iteritems():
-  print dt,"|",val
-print ""
+  print(dt,"|",val)
+print("")
 
 for month in dateutils.month_generator("JAN18","Dec20"):
   put = Option(WTI,month,"put").set_atm()
   atm = put.price()
   put.strike -= 10
   atm10 = put.price()
-  print put.start_date(),"|",put.underlying_price(),"|",atm,"|",atm10
-print ""
+  print(put.start_date(),"|",put.underlying_price(),"|",atm,"|",atm10)
 
-print LIBOR.rates()
-print ""
+print("")
+print(LIBOR.rates())
+print("")
 
-print WTI.mktdata.pricing_date
-print ""
+print(WTI.mktdata.pricing_date)
+print("")
