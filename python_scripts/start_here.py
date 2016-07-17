9e5779d6444d3952b631875d5e733da417ced2b2
Rui Viana <ruilov@gmail.com>
2016-07-17 12:48:50 -0400

    new hedges

--- python_scripts/start_here.py
+++ python_scripts/start_here.py
@@ -5,10 +5,13 @@ from pricer.option import Option
 
 pricer.cme.init()
 
-for month in dateutils.month_generator("JAN17","Dec20"):
-  put = Option(WTI,month,"put")
+for month in dateutils.month_generator("AUG16","Dec19"):
+  put = Option(WTI,month,"put").set_atm()
+  vol = put.vol()
+  put.strike = 40.0
+  p40 = put.price()
+  put.strike = 35.0
+  p35 = put.price()
   put.strike = 30.0
   p30 = put.price()
-  put.strike = 26.0
-  p25 = put.price()
-  print(put.start_date(),"|",put.time_to_expiration(),"|",put.underlying_price(),"|",put.vol(),"|",put.interest_rate(),"|",p25,"|",p30)
+  print put.start_date(),"|",vol,"|",put.underlying_price(),"|",p40,"|",p35,"|",p30
