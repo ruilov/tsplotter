9db3209736219d0befdc16012ce845658248f3f5
Rui Viana <ruilov@gmail.com>
2016-07-24 18:18:28 -0400

    checking the version that's based on implied bs

--- python_scripts/local_vol_learn2.py
+++ python_scripts/local_vol_learn2.py
@@ -2,8 +2,12 @@ import math,numpy as np,random
 from scipy.stats import norm
 
 def volf(logs,t):
-  return 0.4
-  # return .4 - .1*t + math.pow(logs,2)/40
+  # return 0.4
+  return .4 - .1*t + math.pow(logs,2)/40
+
+def total_var(logs,t):
+  vol = volf(logs,t)
+  return vol * vol * t
 
 def cp(strike,t):
   logs = math.log(strike)
@@ -13,10 +17,13 @@ def cp(strike,t):
   p = norm.cdf(d1) - norm.cdf(d2)*strike
   return p
 
+def pp(strike,t):
+  return cp(strike,t) - (1.0 - strike)
+
 num_paths = 10000
 num_strikes = 10
 eps = 1e-3
-dt = 0.1
+dt = 0.01
 high_t = 1.0
 
 for seed in range(1,100):
@@ -33,21 +40,31 @@ for seed in range(1,100):
     local_vols = []
 
     for logs in log_strikes:
-      strike = math.exp(logs)
-      c = cp(strike,t)
-      clow = cp(strike-eps,t)
-      chigh = cp(strike+eps,t)
-      dc_dk = (chigh - c)/eps
-      dc_dk_low = (c - clow)/eps
-      dc_dk2 = (dc_dk - dc_dk_low)/eps
-      c_t_high = cp(strike,t+eps)
-      dc_dt = (c_t_high-c)/eps
-      local_var = dc_dt / (0.5*strike*strike*dc_dk2)
+      # strike = math.exp(logs)
+      # c = cp(strike,t)
+      # clow = cp(strike-eps,t)
+      # chigh = cp(strike+eps,t)
+      # dc_dk = (chigh - c)/eps
+      # dc_dk_low = (c - clow)/eps
+      # dc_dk2 = (dc_dk - dc_dk_low)/eps
+      # c_t_high = cp(strike,t+eps)
+      # dc_dt = (c_t_high-c)/eps
+      # local_var = dc_dt / (0.5*strike*strike*dc_dk2)
+      
+      var_base = total_var(logs,t)
+      var_high = total_var(logs+eps,t)
+      var_low = total_var(logs-eps,t)
+      dw_dy = (var_high - var_base)/eps
+      dw_dy_low = (var_base - var_low)/eps
+      dw_dy2 = (dw_dy - dw_dy_low)/eps
+      var_high_t = total_var(logs,t+eps)
+      dw_dt = (var_high_t - var_base)/eps  
+      z = logs / var_base
+      local_var = dw_dt / (1.0 - z * dw_dy + 0.25 * (-0.25 - 1.0/var_base + z*z) * dw_dy * dw_dy + 0.5 * dw_dy2)
+
       local_vols.append(math.sqrt(local_var))
-      # print t,"|",logs,"|",volf(logs,t),"|",c
 
     local_vols = np.array(local_vols)
-
     rands = [random.gauss(0,1) for x in range(0,num_paths/2)]
     rands = rands + [-x for x in rands]
 
@@ -58,4 +75,4 @@ for seed in range(1,100):
   path_vals = map(math.exp,path_vals)
   strike = 0.8
   pays = [max(strike-x,0) for x in path_vals]
-  print cp(strike,high_t) - 1.0 + strike,"|",np.mean(pays)
+  print pp(strike,high_t),"|",np.mean(pays)
