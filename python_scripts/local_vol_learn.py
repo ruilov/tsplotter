a81dc129a3849a0a1ef3f5a867e02e9c60e5f382
Rui Viana <ruilov@gmail.com>
2016-07-23 21:34:19 -0400

    check point

--- python_scripts/local_vol_learn.py
+++ python_scripts/local_vol_learn.py
@@ -1,121 +1,137 @@
-import lib.dateutils as du,pricer.cme,math,numpy as np,pricer.quant as quant,random,bisect,cProfile,re
+import lib.dateutils as du,pricer.cme,math,numpy as np,random,bisect,cProfile,time
 from pricer.markets import *
 from pricer.option import Option
 from lib.myseries import MySeries
 from scipy.stats import norm
 
-def interp_polys(t,qd,prev_t,prev_poly,next_t,next_poly):
-  if prev_t == next_t: return next_poly(qd)
+def idx_qd(idx,log_strike,vols):
+  return norm.cdf(log_strike / (vols["polys"][idx](0.5) * math.sqrt(vols["ts"][idx])))
 
-  prev_vol = prev_poly(qd)
-  next_vol = next_poly(qd)
-  slope = (next_vol - prev_vol) / (next_t - prev_t)
-  return prev_vol + (t - prev_t) * slope
+def idx_var(idx,log_strike,time,vols):
+  qd = idx_qd(idx,log_strike,vols)
+  vol = vols["polys"][idx](qd)
+  return vol * vol * time
 
-def total_var(log_strike,time,vol_ts,vol_polys):
-  idx = bisect.bisect_left(vol_ts,time)
-  if idx == 0 or idx == len(vol_ts) or vol_ts[idx] == time:
-    if idx == len(vol_ts): idx -= 1
+# calculates the total variance from zero to time, at the given log_strike
+def total_var(log_strike,time,vols):
+  idx = bisect.bisect_left(vols["ts"],time)
+  if idx == 0 or idx == len(vols["ts"]) or vols["ts"][idx] == time:
+    if idx == len(vols["ts"]): idx -= 1
     prev_idx = idx
   else: prev_idx = idx-1
-  atm_vol = interp_polys(time,0.5,vol_ts[prev_idx],vol_polys[prev_idx],vol_ts[idx],vol_polys[idx])
-  qd = norm.cdf(log_strike / (atm_vol * math.sqrt(time)))
-  wing_vol = interp_polys(time,qd,vol_ts[prev_idx],vol_polys[prev_idx],vol_ts[idx],vol_polys[idx])
-  return wing_vol * wing_vol * time
 
-def strike_range(num_strikes,time,vol_ts,vol_polys):
-  tav = math.sqrt(total_var(0.0,time,vol_ts,vol_polys))
-  low_strike = math.exp(-4*tav - tav*tav/2)
-  high_strike = math.exp(3*tav - tav*tav/2)
+  next_var = idx_var(idx,log_strike,time,vols)
+  if idx == prev_idx: return next_var
+
+  prev_var = idx_var(prev_idx,log_strike,time,vols)
+  slope = (next_var - prev_var) / (vols["ts"][idx] - vols["ts"][prev_idx])
+  ans = prev_var + (time - vols["ts"][prev_idx]) * slope
+  return ans
+
+# the range of strikes to calculate local vols in. We could do this for every path value, but that would take forever
+def log_strike_range(num_strikes,time,vols):
+  tav = math.sqrt(total_var(0.0,time,vols))
+  low_strike = -4*tav - tav*tav/2 # make low_strike wide because of high put skews
+  high_strike = 3*tav - tav*tav/2
   delta_strike = (high_strike - low_strike) / (num_strikes-1)
-  strikes = list(np.arange(low_strike,high_strike+delta_strike-1e-7,delta_strike))
+  strikes = np.array(np.arange(low_strike,high_strike+delta_strike-1e-7,delta_strike))
   return strikes
 
-def main():
-  pricer.cme.init()
-
-  option = Option(WTI,"DEC16","put",30.0)
-
+# the following math is described in http://www.math.ku.dk/~rolf/teaching/ctff03/Gatheral.1.pdf
+def local_vol_calc(log_strike,time,vols):
   eps = 1e-4
-  today = WTI.mktdata.pricing_date
-  lastT = du.dt(today,option.expiration_date())
-  dt = lastT / 100
-  num_strikes = 500
+  var_base = total_var(log_strike,time,vols)
+  var_high = total_var(log_strike+eps,time,vols)
+  var_low = total_var(log_strike-eps,time,vols)
+  
+  dw_dy = (var_high - var_base)/eps
+  dw_dy_low = (var_base - var_low)/eps
+  dw_dy2 = (dw_dy - dw_dy_low)/eps
+  
+  var_high_t = total_var(log_strike,time+eps,vols)
+  dw_dt = (var_high_t - var_base)/eps
 
-  times = list(np.arange(dt,lastT + dt - 1e-7,dt))
-  local_vols = np.zeros((len(times),num_strikes))
-  cum_probs = np.zeros((len(times),num_strikes)) # this is for sanity checking
-  strikes = np.zeros((len(times),num_strikes)) # this is for sanity checking
+  # if dw_dt < 0: return 0.0  # negative forward variance!
+  
+  z = log_strike / var_base
+  local_var = dw_dt / (1.0 - z * dw_dy + 0.25 * (-0.25 - 1.0/var_base + z*z) * dw_dy * dw_dy + 0.5 * dw_dy2)
+  local_vol = math.sqrt(local_var)
+  return local_vol
 
+# these are the days we'll actually simulate. The argument sim_days are the days the user wants to observe
+# note that 'days' refers to the number of days from today to that date
+def make_little_sim_days(sim_days,max_days=1):
+  little_sim_days = list(range(max_days,sim_days[-1]+max_days,max_days))
+  for day in sim_days:
+    idx = bisect.bisect_left(little_sim_days,day)
+    if little_sim_days[idx] != day: little_sim_days.insert(idx,day)
+  return little_sim_days
+
+def calib_local_vols(mkt,sim_dates):
   vol_series = WTI.vols()
-  vol_ts = [du.dt(today,x) for x in vol_series.index]
+  vol_ts = [du.dt(mkt.mktdata.pricing_date,x) for x in vol_series.index]
   vol_polys = vol_series.values
-  # vol_idxs = list(range(0,len(vol_ts)))
+  vols = {"ts": vol_ts, "polys": vol_polys}
+  num_strikes = 10
+  answer = []
 
-  for ti,time in enumerate(times):
-    srange = strike_range(num_strikes,time,vol_ts,vol_polys)
-    for si,strike in enumerate(srange):
-      strikes[ti][si] = strike
+  sim_days = [(x-mkt.mktdata.pricing_date).days for x in sim_dates]
+  little_sim_days = make_little_sim_days(sim_days)
+  last_sim_day = 0
+  for sim_day in little_sim_days:
+    print sim_day
+    time = float(sim_day)/365
+    dt = float(sim_day - last_sim_day) / 365
+    last_sim_day = sim_day
 
-      # the following math is described in http://www.math.ku.dk/~rolf/teaching/ctff03/Gatheral.1.pdf
-      log_strike = math.log(strike)
-      var_base = total_var(log_strike,time,vol_ts,vol_polys)
-      var_high = total_var(log_strike+eps,time,vol_ts,vol_polys)
-      var_low = total_var(log_strike-eps,time,vol_ts,vol_polys)
-      
-      dw_dy = (var_high - var_base)/eps
-      dw_dy_low = (var_base - var_low)/eps
-      dw_dy2 = (dw_dy - dw_dy_low)/eps
-      
-      var_high_t = total_var(log_strike,time+eps,vol_ts,vol_polys)
-      dw_dt = (var_high_t - var_base)/eps
-      
-      z = log_strike / var_base
-      local_var = dw_dt / (1.0 - z * dw_dy + 0.25 * (-0.25 - 1.0/var_base + z*z) * dw_dy * dw_dy + 0.5 * dw_dy2)
-      local_vol = math.sqrt(local_var)
-      local_vols[ti][si] = local_vol
-
-      # this is for sanity checking
-      strike_high = math.exp(log_strike+eps)
-      dstrike = strike_high - strike
-      v_base = math.sqrt(var_base/time)
-      v_high = math.sqrt(var_high/time)
-      p1 = quant.blackScholes_price("call",1.0,strike,time,v_base,0.0)
-      p2 = quant.blackScholes_price("call",1.0,strike_high,time,v_high,0.0)
-      dprice_dk = (p2-p1)/dstrike
-      cum_probs[ti][si] = 1.0+dprice_dk
-
-  # ======================================================================
-  random.seed(6)
-
-  num_paths = 30000
-  strike_idxs = list(range(0,num_strikes))
-  hist = [0] * num_strikes
-  pays = []
-
-  path_vals = [1.0] * num_paths
-  for ti in range(0,len(times)):
+    small_dt = dt/1
+    for time2 in np.arange(time - dt + small_dt,time + small_dt - 1e-6,small_dt):   
+      log_strikes = log_strike_range(num_strikes,time2,vols)
+      local_vols = np.array([local_vol_calc(x,time2,vols) for x in log_strikes])
+      record = sim_day in sim_days and abs(time-time2)<1e-6
+      answer.append({"dt": small_dt, "log_strikes": log_strikes, "local_vols": local_vols, "record": record})
+  return answer
+  
+def monte_carlo(local_vols_arr,num_paths):
+  eps = 1e-4
+  path_vals = [0.0] * num_paths
+  answer = []
+  for elem in local_vols_arr:
+    [dt,log_strikes,local_vols,record] = [elem["dt"],elem["log_strikes"],elem["local_vols"],elem["record"]]
     rands = [random.gauss(0,1) for x in range(0,num_paths/2)]
     rands = rands + [-x for x in rands]
+
     for path_num in range(0,num_paths):
-      local_vol = np.interp(path_vals[path_num],strikes[ti],local_vols[ti]) * math.sqrt(dt)
-      path_vals[path_num] *= math.exp(rands[path_num]*local_vol - local_vol * local_vol / 2.0)
+      local_vol = np.interp(path_vals[path_num],log_strikes,local_vols)
+      local_vol_high = np.interp(path_vals[path_num]+eps,log_strikes,local_vols)
+      local_vol_deriv = (local_vol_high-local_vol)/eps
 
-  for path_num,path_val in enumerate(path_vals):  
-    pays.append(max(option.strike - path_val*option.underlying_price(),0))
-    si = int(math.ceil(np.interp(path_val,strikes[-1],strike_idxs)))
-    hist[si] += 1
+      inc_f = rands[path_num]*local_vol * math.sqrt(dt) - local_vol * local_vol * dt / 2.0
+      # the log-milstein time-discretization correction
+      # see: http://papers.ssrn.com/sol3/papers.cfm?abstract_id=2175090
+      inc_f += 0.5 * local_vol * local_vol_deriv * (rands[path_num]*rands[path_num]-1)*dt
+      path_vals[path_num] += inc_f
+    if record: answer.append([math.exp(x) for x in path_vals])
+  return answer
+    
+def main():
+  pricer.cme.init()
+  num_paths = 10000
 
-  cum_hist = [0] * len(hist)
-  c = 0.0
-  for i,h in enumerate(hist):
-    c += h
-    cum_hist[i] = c / num_paths
+  opt = Option(WTI,"DEC16","put",30.0)
+  local_vols_arr = calib_local_vols(WTI,[opt.expiration_date()])
 
-  for i,s in enumerate(strikes[-1]):
-    print s,"|",cum_probs[-1][i],"|",cum_hist[i]
-
-  print option.price(),"|",np.mean(pays)
+  # for dt,val in WTI.vols().iteritems(): print du.dt(WTI.mktdata.pricing_date,dt),"|",val(0.5)
+  
+  t1 = time.time()
+  for seed in range(1,20):
+    random.seed(seed)
+    paths = monte_carlo(local_vols_arr,num_paths)
+    pays = [max(opt.strike - x * opt.underlying_price(),0) for x in paths[0]]
+    t2 = time.time()
+    runt = (t2-t1)
+    t1 = t2
+    print opt.price()/opt.discount_factor(),"|",np.mean(pays),"|",runt
 
 main()
 
